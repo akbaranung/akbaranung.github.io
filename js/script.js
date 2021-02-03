7ec59a8e308a101740b4b1215ba9ad8e1fd945e6
Akbar Anung <59117786+akbaranung@users.noreply.github.com>
2021-02-03 15:45:31 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -16,4 +16,26 @@ M.Parallax.init(parallax);
 var preloader = document.getElementById('loading');
 function myFunction(){
 	preloader.style.display='none';
-}
+};
+
+const sr = ScrollReveal({
+    origin : 'top',
+    distance: '100px',
+    duration: 2000,
+    reset: true
+})
+
+sr.reveal('.title h2', {})
+sr.reveal('.title p', {origin:'top', delay:200})
+sr.reveal('.title .button', {origin:'right', delay:400})
+
+
+sr.reveal('.about img', {origin: 'left', delay:500})
+sr.reveal('.about h5', {origin:'top', delay:300})
+sr.reveal('.about p', {origin:'top', delay:400})
+
+sr.reveal('.skills img', {interval:200})
+
+sr.reveal('.portfolio img', {origin:'left', interval:200})
+
+sr.reveal('.contact a', {interval:200})
